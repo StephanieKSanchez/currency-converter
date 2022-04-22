c165b7cb80588ecbf7dfbe6193387611d001267e
Stephanie Sanchez <stephaniesanchez@Stephanies-MacBook-Air-2.local>
2022-04-22 11:12:58 -0400

    currency converter

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function converted(usDollar, thaiBaht){
     return usDollar * thaiBaht
 }
 
-let result = converted(usDollar, thaiBaht)
+const result = converted(usDollar, thaiBaht)
 
 console.log(usDollar + ' in USD is ' + result + ' Thai Bhat')
